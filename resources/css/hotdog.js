07d68392db1abfa60dde252ab9985e79fc757248
Misty Tomlin <46390494+MistyCanCode@users.noreply.github.com>
2020-08-22 22:37:27 -0500

    Add files via upload
    initial upload

---
+++ resources/css/hotdog.js
@@ -0,0 +1,19 @@
+//need to animate four different divs with reviews in them
+//4 numbers to a case statement that will put that string inside one of the divs
+//and make it change every few seconds
+
+$(document).ready(function() {
+	$("button").click(function(){
+
+document.getElementById("#review1");
+$("#review1").fadeIn(3000).fadeOut(3000);
+document.getElementById("#review2");
+$("#review2").fadeIn(6000).fadeOut(6000);
+document.getElementById("#review3");
+$("#review3").fadeIn(9000).fadeOut(9000);
+document.getElementById("#review4");
+$("#review4").fadeIn(12000).fadeOut(12000);
+
+})
+
+});
